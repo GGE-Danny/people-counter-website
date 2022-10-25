c10a61f4d13367f1805df3d7bd78f7efb1c727ee
GGE-Danny <102856204+GGE-Danny@users.noreply.github.com>
2022-10-25 22:14:07 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment(){
+    count+=1;
+    countEl.innerText= count;
+    
+}
+
+function save(){
+    let countStr = " "+ count+ " - "
+    saveEl.textContent += countStr;
+    countEl.textContent=0;
+    count = 0;
+
+
+    console.log(count);
+}
+
+// let name= "Daniel";
+// let greeting = " Hi, my name is "
+
+// myGreeting =  greeting+name ;
+
+// console.log(myGreeting)
+
+
+
